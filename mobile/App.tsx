ec7de0ffc7bd2eb45e5663cb2cbe30ae242d93cb
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-09-12 22:00:26 -0300

    organizando o ambiente

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+
+      <Button title='Send 1' />
+      <Button title='Send 2' />
+      <Button title='Send 3' />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
