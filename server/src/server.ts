5bdbd06da80179abf9cb934c5b48dd2de1eb8c27
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-09-14 22:02:04 -0300

    criação do banco de dados e rotas pra api

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,29 @@ import express from 'express';
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+
     return response.json([
         {id: 1, nome: 'Anuncio 1'},
         {id: 2, nome: 'Anuncio 2'},
         {id: 3, nome: 'Anuncio 3'},
         {id: 4, nome: 'Anuncio 4'}
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    const adId = request.params.id;
+    
+    return response.json([])
 })
 
 app.listen(3333)
